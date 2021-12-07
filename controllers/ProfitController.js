4bd49da1e7b56dd5d5126f9e55c6bb37d54a9667
alihussainkabri <alihussainkabri52@gmail.com>
2021-12-07 11:50:29 +0530

    sales changes

--- controllers/ProfitController.js
+++ controllers/ProfitController.js
@@ -14,34 +14,31 @@ router.getProfitAmountReport = async (req, res) => {
     let vehicle_sales = 0;
     let garage_sales = 0;
 
-    if (req.user_data.role == 2){
-        await knex('sale_start').where('restaurent_id', req.user_data.restaurent_id).where('sale_date', current_date).where("status",1).then(response => {
-            if (response) {
-                status = 200;
-                message = 'sales record has been fetched successfully!';
-    
-                for (let i = 0; i < response.length; i++) {
-                    sales_record = parseFloat(sales_record) + parseFloat(response[i].total_after_roundoff)
-                }
-            }
-        }).catch(err => console.log(err))
-    
-        await knex('expenses').where('restaurent_id', req.user_data.restaurent_id).where('expense_date', current_date).then(response => {
-            if (response) {
-                status = 200;
-                message = 'expense record has been fetched successfully!';
-    
-                for (let i = 0; i < response.length; i++) {
-                    expense_record = parseFloat(expense_record) + parseFloat(response[i].paid_amount)
-                }
-            }
-        }).catch(err => console.log(err))
-    }else{
-        status = 500;
-        message = "You don't have permission"
-    }
 
-    
+    await knex('sale_start').where('restaurent_id', req.user_data.restaurent_id).where('sale_date', current_date).where("status", 1).then(response => {
+        if (response) {
+            status = 200;
+            message = 'sales record has been fetched successfully!';
+
+            for (let i = 0; i < response.length; i++) {
+                sales_record = parseFloat(sales_record) + parseFloat(response[i].total_after_roundoff)
+            }
+        }
+    }).catch(err => console.log(err))
+
+    await knex('expenses').where('restaurent_id', req.user_data.restaurent_id).where('expense_date', current_date).then(response => {
+        if (response) {
+            status = 200;
+            message = 'expense record has been fetched successfully!';
+
+            for (let i = 0; i < response.length; i++) {
+                expense_record = parseFloat(expense_record) + parseFloat(response[i].paid_amount)
+            }
+        }
+    }).catch(err => console.log(err))
+
+
+
 
     return res.json({ status, message, sales_record, expense_record })
 }
@@ -61,32 +58,29 @@ router.filterDateProfit = async (req, res) => {
     let sales_query = `select * from sale_start where sale_start.status=1 sale_start.restaurent_id='${req.user_data.restaurent_id}' and sale_start.sale_date BETWEEN '${inputs.from}' AND '${inputs.to}'`
     let expense_query = `select * from expenses where expenses.restaurent_id='${req.user_data.restaurent_id}' and expenses.expense_date BETWEEN '${inputs.from}' AND '${inputs.to}'`
 
-    if(req.user_data.role == 2){
-        await knex.raw(sales_query).then(response => {
-            if (response[0]) {
-                status = 200;
-                message = 'sales record has been fetched successfully!';
 
-                for (let i = 0; i < response[0].length; i++) {
-                    sales_record = parseFloat(sales_record) + parseFloat(response[0][i].total_after_roundoff)
-                }
+    await knex.raw(sales_query).then(response => {
+        if (response[0]) {
+            status = 200;
+            message = 'sales record has been fetched successfully!';
+
+            for (let i = 0; i < response[0].length; i++) {
+                sales_record = parseFloat(sales_record) + parseFloat(response[0][i].total_after_roundoff)
             }
-        }).catch(err => console.log(err))
+        }
+    }).catch(err => console.log(err))
 
-        await knex.raw(expense_query).then(response => {
-            if (response[0]) {
-                status = 200;
-                message = 'expense record has been fetched successfully!';
+    await knex.raw(expense_query).then(response => {
+        if (response[0]) {
+            status = 200;
+            message = 'expense record has been fetched successfully!';
 
-                for (let i = 0; i < response[0].length; i++) {
-                    expense_record = parseFloat(expense_record) + parseFloat(response[0][i].paid_amount)
-                }
+            for (let i = 0; i < response[0].length; i++) {
+                expense_record = parseFloat(expense_record) + parseFloat(response[0][i].paid_amount)
             }
-        }).catch(err => console.log(err))
-    }else{
-        status = 500;
-        message = "You don't have permission"
-    }
+        }
+    }).catch(err => console.log(err))
+
 
     return res.json({ status, message, sales_record, expense_record })
 }
